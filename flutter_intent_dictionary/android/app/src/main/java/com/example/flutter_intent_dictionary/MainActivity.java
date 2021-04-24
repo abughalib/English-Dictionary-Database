518966633d92d4fec0863aa33542111cd7754df8
abughalib <abughalib64@gmail.com>
2021-04-25 00:42:04 +0530

    added flutter test app

---
+++ flutter_intent_dictionary/android/app/src/main/java/com/example/flutter_intent_dictionary/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.flutter_intent_dictionary;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ flutter_intent_dictionary/assets/dict_sqlite.py
@@ -0,0 +1,21 @@
+import sqlite3
+import time
+
+conn = sqlite3.connect('/home/abughalib/Downloads/dictionary.db')
+
+cur = conn.cursor()
+
+input_word = input("Enter Word: ")
+
+while(input_word != 'exit'):
+
+    meaning = conn.execute(
+        f'SELECT * FROM dict WHERE word="{input_word}"'
+    )
+
+    if cur is None:
+        print("Meaning not found!")
+    else:
+        for i in meaning:
+            print(i)
+    input_word = input("Enter Word: ")
