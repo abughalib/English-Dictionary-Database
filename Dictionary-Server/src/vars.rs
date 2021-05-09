7329a33dd91d16257b5cb6c2d364d360c7c7c4fe
abughalib <abughalib64@gmail.com>
2021-05-09 22:41:47 +0530

    database user & pass from .env

--- Dictionary-Server/src/vars.rs
+++ Dictionary-Server/src/vars.rs
@@ -4,7 +4,25 @@ use dotenv::dotenv;
 
 pub fn get_database_url()->String{
   dotenv().ok();
-  
-  env::var("DATABASE_URL")
-  .expect("DATABASE_URL must be set to .env")
+  let url = env::var("DB_URL")
+  .expect("DATABASE_URL must be set to .env");
+
+  let database_url = String::from(
+    format!(
+      "postgres://{}:{}@{}", get_database_user(), 
+      get_database_password(), url
+    ));
+  return database_url;
+}
+
+pub fn get_database_user()->String{
+  dotenv().ok();
+  env::var("DB_USER")
+    .expect("DATABASE_USER must be set in .env")
+}
+
+pub fn get_database_password()->String{
+  dotenv().ok();
+  env::var("DB_PASS")
+    .expect("DATABASE_PASSWORD must be set in .env")
 }
