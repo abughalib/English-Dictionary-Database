79287d63449a089827c1266e925952e8b9f622ae
Abu Ghalib <abughalib64@gmailcom>
2023-01-10 12:22:55 +0530

    added logger

--- src/bin/run.rs
+++ src/bin/run.rs
@@ -3,14 +3,20 @@ extern crate dictionary_server;
 extern crate dotenv;
 
 use actix_cors::Cors;
+use actix_web::middleware::Logger;
 use actix_web::{guard, web, App, HttpServer};
 use dictionary_server::routes::{help, index, page_not_found, query_meaning, query_words};
 use dictionary_server::vars::get_host_path;
+use env_logger::Env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
     HttpServer::new(|| {
         App::new()
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
             .wrap(
                 Cors::default()
                     .allow_any_origin()
@@ -25,7 +31,7 @@ async fn main() -> std::io::Result<()> {
                     .guard(guard::Header("content-type", "application/json"))
                     .route("api", web::post().to(query_meaning))
                     .guard(guard::Header("content-type", "application/json"))
-                    .route("search", web::post().to(query_words))
+                    .route("search", web::post().to(query_words)),
             )
             .default_service(
                 web::route()
